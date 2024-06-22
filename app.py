ed8d0061009ad140c05fdf45140f7694877f2185
Aryaman Somvanshi <aryaman1505@gmail.com>
2024-06-22 18:58:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,5 @@ def upload_file():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(port='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
